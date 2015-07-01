b07b16a576f511635654ddf23f28f87c904c702c
Rakesh.P.D <errakeshpd@gmail.com>
2015-07-01 20:00:20 +0530

    tiny url redirecton if url match with the already created tiny urls

--- app/controllers/tinyurls_controller.rb
+++ app/controllers/tinyurls_controller.rb
@@ -3,15 +3,25 @@ class TinyurlsController < ApplicationController
   	@urls = Tinyurl.all
   end
   def tinyurl
-  	a = randomurl()
   	@data = Tinyurl.new
   	@data.user_url = params[:url]
-  	@data.tiny_url = a
+  	@data.tiny_url = request.protocol + request.host_with_port + '/' + randomurl()
   	@data.save
   	redirect_to :back
   end
   def randomurl
   	obj = [('a'..'z'), ('A'..'Z'), (1..9)].map { |i| i.to_a }.flatten
 	string = (0...5).map { obj[rand(obj.length)] }.join
+  end
+  def siteredirect
+    @data = params[:rest]
+    @data = request.protocol + request.host_with_port + '/' + @data 
+    @userdata = Tinyurl.find_by_tiny_url(@data)
+    if @userdata
+       redirect_to @userdata.user_url 
+    else
+       redirect_to root_path
+    end
+
   end
 end
