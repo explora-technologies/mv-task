1502b1b669b06d1621422e13efdb8deef5d74a1d
Rakesh.P.D <errakeshpd@gmail.com>
2015-07-01 09:21:25 +0530

    tiny url post action modified, routes specified

--- app/controllers/tinyurls_controller.rb
+++ app/controllers/tinyurls_controller.rb
@@ -3,6 +3,10 @@ class TinyurlsController < ApplicationController
   	@tinies = Tinyurl.all
   end
   def tinyurl
-  	sdfgsdf
+  	a = randomurl()
+  end
+  def randomurl
+  	obj = [('a'..'z'), ('A'..'Z'), (1..9)].map { |i| i.to_a }.flatten
+	string = (0...5).map { obj[rand(obj.length)] }.join
   end
 end
